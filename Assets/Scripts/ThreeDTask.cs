cb30f968c9bcc6d1984894dc9e9dca7e6dcc316b
RoberNagy <robernagy02@gmail.com>
2026-01-05 10:52:20 +0200

    3dtasks

---
+++ Assets/Scripts/ThreeDTask.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class ThreeDTask : MonoBehaviour
+{
+    private float x;
+    private float z;
+    CharacterController characterController;
+    private float speed = 5.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x =Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        characterController.Move(move * speed * Time.deltaTime);
+
+    }
+}
